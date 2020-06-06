c2bdaab4e4f63590a0338eaf5a5d360026a9016b
Felipe <felipe@aifund.ai>
2020-06-05 20:06:37 -0500

    Styled antileaderboard

--- src/components/Reasons/Reasons.js
+++ src/components/Reasons/Reasons.js
@@ -5,7 +5,6 @@ const Container = styled.div`
   flex-grow: 1;
   border: 2px dashed ${p => p.theme.colors.lightgreen};
   border-radius: 5px;
-  margin-left: 30px;
   padding: 20px;
 `
 
@@ -17,8 +16,6 @@ color: ${p => p.theme.colors.palegreen}
 
 const Arguments = styled.p`
   color: ${p => p.theme.colors.lightgreen};
-  max-width: 400px;
-  min-width: 300px;
   font-weight: 400;
   a {
     color: ${p => p.theme.colors.palegreen}
@@ -34,11 +31,7 @@ const Reasons = () => {
                 focus on highly demanding tasks. Taking a simple break helps us regain focus and prevents exhaustion and fatigue.
             </Arguments>
             <Arguments>
-                Here are some articles that give more insight into the benefits of taking regular breaks.
-                <ul>
-                    <li><a target="_blank" rel="noopener" href="https://www.fastcompany.com/3034928/why-you-need-to-stop-thinking-you-are-too-busy-to-take-breaks">Why you need to stop thinking you are too busy to take breaks.</a></li>
-                    <li><a target="_blank" rel="noopener" href="https://www.psychologytoday.com/us/blog/changepower/201704/how-do-work-breaks-help-your-brain-5-surprising-answers">Although a bit "clickbaity", this one makes an effort to reference actual studies.</a></li>
-                </ul>
+              <a target="_blank" rel="noopener" href="https://www.fastcompany.com/3034928/why-you-need-to-stop-thinking-you-are-too-busy-to-take-breaks">Why you need to stop thinking you are too busy to take breaks.</a>
             </Arguments>
         </Container>
     )
--- src/containers/session/Session.js
+++ src/containers/session/Session.js
@@ -9,7 +9,7 @@ import RoutineStep from '../../components/routineStep/RoutineStep'
 import ActivityLog from '../../components/activityLog/ActivityLog'
 import RoutineActions from '../../components/RoutineActions/RoutineActions'
 import Reasons from '../../components/Reasons/Reasons'
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
 import SessionLoader from '../../components/SessionLoader/SessionLoader'
 import AntiLeaderBoard from '../../components/AntiLeaderBoard/AntiLeaderBoard'
 
@@ -34,102 +34,111 @@ const Goals = styled.div`
   justify-content: space-between;
 `
 
+const LeftColumn = styled.div`
+  flex: 1;
+  max-width: 330px;
+`
+
 const Footer = styled.div`
  height: 200px;
 `
 
 const Session = () => {
 
-    const [currentUserEmail, setCurrentUserEmail] = useState('')
-    const [currentUsername, setCurrentUsername] = useState('')
+  const [currentUserEmail, setCurrentUserEmail] = useState('')
+  const [currentUsername, setCurrentUsername] = useState('')
 
-    const [currentRoutine, setCurrentRoutine] = useState(null)
-    const [currentStep, setCurrentStep] = useState(0)
+  const [currentRoutine, setCurrentRoutine] = useState(null)
+  const [currentStep, setCurrentStep] = useState(0)
 
-    const [userSessions, setUserSessions] = useState([])
+  const [userSessions, setUserSessions] = useState([])
 
-    const [sessionCompleted, setSessionCompleted] = useState(false)
+  const [sessionCompleted, setSessionCompleted] = useState(false)
 
-    const [completingSession, setCompletingSession] = useState(false)
+  const [completingSession, setCompletingSession] = useState(false)
 
-    const startSession = async (email) => {
-        await BreakService.startSession({ email, started: true })
-    }
+  const startSession = async (email) => {
+    await BreakService.startSession({ email, started: true })
+  }
 
-    const retrieveUserSessions = async (email) => {
-        setCompletingSession(true)
-        const sessions = await BreakService.getUserSessions(email)
-        if(sessions && sessions.length > 0) {
-            const sortedSessions = sessions.sort((a, b) => {
-                const aDate = new Date(a.date)
-                const bDate = new Date(b.date)
-                return aDate.getTime() - bDate.getTime()
-            })
+  const retrieveUserSessions = async (email) => {
+    setCompletingSession(true)
+    const sessions = await BreakService.getUserSessions(email)
+    if (sessions && sessions.length > 0) {
+      const sortedSessions = sessions.sort((a, b) => {
+        const aDate = new Date(a.date)
+        const bDate = new Date(b.date)
+        return aDate.getTime() - bDate.getTime()
+      })
 
-            const lastSession = sortedSessions[sortedSessions.length-1]
-            const lastSessionDate = new Date(lastSession.date)
-            const lastSessionDay = lastSessionDate.getUTCDate()
-            const todayDay = new Date().getUTCDate()
-            //TODO: Check dates at midnight
-            if (lastSessionDay === todayDay) {
-                setSessionCompleted(true)
-            }
-        }
-        setUserSessions(sessions)
-        setCompletingSession(false)
-    }
-
-    useEffect(() => {
-        const user = StorageService.getCurrentUser()
-        setCurrentUsername(user.displayName)
-        setCurrentUserEmail(user.email)
-        if(user.email) {
-            retrieveUserSessions(user.email)
-            startSession(user.email)
-        }
-    }, [])
-
-    useEffect(() => {
-        const getRoutine = async () => {
-            const routine = await BreakService.getTodayRoutine()
-            setCurrentRoutine(routine)
-        }
-        getRoutine()
-    }, [])
-
-    const onFinisSession = async () => {
-        setCompletingSession(true)
-        await BreakService.completeSession({ email: currentUserEmail, completed: true })
+      const lastSession = sortedSessions[sortedSessions.length - 1]
+      const lastSessionDate = new Date(lastSession.date)
+      const lastSessionDay = lastSessionDate.getUTCDate()
+      const todayDay = new Date().getUTCDate()
+      //TODO: Check dates at midnight
+      if (lastSessionDay === todayDay) {
         setSessionCompleted(true)
-        setCompletingSession(false)
-        retrieveUserSessions(currentUserEmail)
+      }
     }
+    setUserSessions(sessions)
+    setCompletingSession(false)
+  }
 
-    if (!currentRoutine) {
-        return <SessionLoader/>
+  useEffect(() => {
+    const user = StorageService.getCurrentUser()
+    setCurrentUsername(user.displayName)
+    setCurrentUserEmail(user.email)
+    if (user.email) {
+      retrieveUserSessions(user.email)
+      startSession(user.email)
     }
+  }, [])
 
-    const steps = currentRoutine.resources.map((step, i) => {
-        return <RoutineStep show={currentStep == i} key={i} step={step}></RoutineStep>
-    })
+  useEffect(() => {
+    const getRoutine = async () => {
+      const routine = await BreakService.getTodayRoutine()
+      setCurrentRoutine(routine)
+    }
+    getRoutine()
+  }, [])
 
-    return (
-        <SessionContainer>
-            <Header>
-                <BreakMessage message={`${currentUsername}, take a break.`}/>
-                <Timer/>
-            </Header>
-            <Routine>
-                {steps}
-            </Routine>
-            <RoutineActions loading={completingSession} finishSession={onFinisSession} sessionCompleted={sessionCompleted}></RoutineActions>
-            <Goals>
-                <ActivityLog sessions={userSessions}/>
-                <AntiLeaderBoard />
-            </Goals>
-            <Footer></Footer>
-        </SessionContainer>
-    )
+  const onFinisSession = async () => {
+    setCompletingSession(true)
+    await BreakService.completeSession({ email: currentUserEmail, completed: true })
+    setSessionCompleted(true)
+    setCompletingSession(false)
+    retrieveUserSessions(currentUserEmail)
+  }
+
+  if (!currentRoutine) {
+    return <SessionLoader/>
+  }
+
+  const steps = currentRoutine.resources.map((step, i) => {
+    return <RoutineStep show={currentStep == i} key={i} step={step}></RoutineStep>
+  })
+
+  return (
+    <SessionContainer>
+      <Header>
+        <BreakMessage message={`${currentUsername}, take a break.`}/>
+        <Timer/>
+      </Header>
+      <Routine>
+        {steps}
+      </Routine>
+      <RoutineActions loading={completingSession} finishSession={onFinisSession}
+                      sessionCompleted={sessionCompleted}></RoutineActions>
+      <Goals>
+        <LeftColumn>
+          <ActivityLog sessions={userSessions}/>
+          <Reasons/>
+        </LeftColumn>
+        <AntiLeaderBoard/>
+      </Goals>
+      <Footer></Footer>
+    </SessionContainer>
+  )
 }
 
 export default Session
